5ec13881d1f30a2936a7f7c086ac8b22cef81ec1
Tomás Carignano Aznarez <75227199+tomascarignano98@users.noreply.github.com>
2021-01-07 17:30:46 -0300

    Add files via upload

---
+++ 9. Python Standard Library/8. Working with a SQLite Database/app2.py
@@ -0,0 +1,10 @@
+# Reading an SQLite Database
+import sqlite3
+import json
+from pathlib import Path
+
+with sqlite3.connect("db.sqlite3") as conn:
+    command = "SELECT * FROM Movies"
+    cursor = conn.execute(command)  # iterable object
+    for row in cursor:
+        print(row)
