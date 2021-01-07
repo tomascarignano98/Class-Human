8eec03841f2ee4cd4175b04efda3c8b00d87c60a
Tomás Carignano Aznarez <75227199+tomascarignano98@users.noreply.github.com>
2021-01-07 17:22:58 -0300

    Add files via upload

---
+++ 9. Python Standard Library/app.py
@@ -0,0 +1,13 @@
+import sqlite3
+import json
+from pathlib import Path
+
+movies = json.loads(Path("movies.json").read_text())
+# now we want to store this list in a database.
+
+# this connect method returns a connection object.
+with sqlite3.connect("db.sqlite3") as conn:
+    command = "INSERT INTO Movies VALUES(?, ?, ?)"
+    for movie in movies:
+        conn.execute(command, tuple(movie.values()))
+    conn.commit()
