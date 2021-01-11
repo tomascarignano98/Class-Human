f2e693c62f3b9f13b5d5c2f311a712b492342f50
Tomás Carignano Aznarez <75227199+tomascarignano98@users.noreply.github.com>
2021-01-11 16:32:10 -0300

    Add files via upload

---
+++ 9. Python Standard Library/app.py
@@ -0,0 +1,9 @@
+import webbrowser
+
+print("Deployment completed")
+webbrowser.open("http://google.com")
+
+"""
+Useful when you create an automation script and at the end you 
+want to open a browser window. So instead of typing manually, automate it
+"""
